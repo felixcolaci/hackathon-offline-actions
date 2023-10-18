a06546f214aae845290875975a1669a533eb0004
Michael Pattison <michael.pattison@auth0.com>
2023-10-18 14:45:41 +0200

    WIP: 7b7fd5c Added JSDocs Fixed some type issues Added blanket attribute to Event object due to multiple undocumented attributes

---
+++ types/API/CredentialExchangeAPI.ts
@@ -0,0 +1,12 @@
+import { ApiCache } from "./types/APICache"
+import { ApiAccess } from "./types/ApiAccess"
+import { ApiAccessToken } from "./types/ApiAccessToken"
+
+
+export type ApiAccessTokenCC = Omit<ApiAccessToken, 'addScope' | 'removeScope'>;
+
+export interface CredentialExchangeAPI {
+    access: ApiAccess,
+    accessToken: ApiAccessTokenCC,
+    cache: ApiCache,
+}
