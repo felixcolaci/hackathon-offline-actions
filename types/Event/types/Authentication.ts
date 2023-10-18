abbd31dd3a5548eaf1997230dee24d9c49753dd8
Felix Colaci <felix.colaci@okta.com>
2023-10-18 10:41:07 +0200

    patch authentication with primitive type

--- types/Event/types/Authentication.ts
+++ types/Event/types/Authentication.ts
@@ -2,79 +2,83 @@
  * Details about authentication signals obtained during the login flow.
  */
 export interface EventAuthentication {
-    /**
-     * Contains the authentication methods a user has completed during their session.
-     */
-    methods: Array<AuthenticationMethod>,
-    /**
-     * Details about risk assessments obtained during the login flow.
-     */
-    riskAssessment: RiskAssessment | undefined,
-    confidence: String,
-    version: String
+  /**
+   * Contains the authentication methods a user has completed during their session.
+   */
+  methods: Array<AuthenticationMethod>;
+  /**
+   * Details about risk assessments obtained during the login flow.
+   */
+  riskAssessment: RiskAssessment | undefined;
+  confidence: string;
+  version: string;
 }
 /**
  * Contains the authentication methods a user has completed during their session.
  */
 interface AuthenticationMethod {
-    name: "federated" | "passkey" | "pwd" | "sms" | "email" | "mfa" | "mock" | any,
-    timestamp: String,
-    /**
-     * A specific MFA factor. Only present when name is set to mfa.
-     */
-    type: string | undefined
+  name: "federated" | "passkey" | "pwd" | "sms" | "email" | "mfa" | "mock" | any;
+  timestamp: string;
+  /**
+   * A specific MFA factor. Only present when name is set to mfa.
+   */
+  type: string | undefined;
 }
 
 /**
  * Details about risk assessments obtained during the login flow.
  */
 interface RiskAssessment {
-    assessments: {
-        /**
-         * Determines if the user is logging in from a location signaling impossible travel.
-         */
-        ImpossibleTravel: ImpossibleTravelAssessment | undefined,
-        /**
-         * Determines if the user is logging in from a known device.
-         */
-        NewDevice: NewDeviceAssessment | undefined,
-        /**
-         * Shows if the IP was found in Auth0's repository of low reputation IPs.
-         */
-        UntrustedIP: UntrustedIPAssessment | undefined
-    }
+  assessments: {
+    /**
+     * Determines if the user is logging in from a location signaling impossible travel.
+     */
+    ImpossibleTravel: ImpossibleTravelAssessment | undefined;
+    /**
+     * Determines if the user is logging in from a known device.
+     */
+    NewDevice: NewDeviceAssessment | undefined;
+    /**
+     * Shows if the IP was found in Auth0's repository of low reputation IPs.
+     */
+    UntrustedIP: UntrustedIPAssessment | undefined;
+  };
 }
 
 /**
  * Determines if the user is logging in from a location signaling impossible travel.
  */
 interface ImpossibleTravelAssessment {
-    code: String,
-    confidence: String
+  code: string;
+  confidence: string;
 }
 
 /**
  * Determines if the user is logging in from a known device.
  */
 interface NewDeviceAssessment {
-    code: String,
-    confidence: String,
-    details: {
-        device: String | undefined,
-        useragent: String | undefined
-    } | undefined
+  code: string;
+  confidence: string;
+  details:
+    | {
+        device: string | undefined;
+        useragent: string | undefined;
+      }
+    | undefined;
 }
 
 /**
  * Shows if the IP was found in Auth0's repository of low reputation IPs.
  */
 interface UntrustedIPAssessment {
-    code: String,
-    confidence: String,
-    details: {
-        category: String | undefined,
-        ip: String | undefined,
-        matches: String | undefined,
-        source: String | undefined
-    } | undefined
+  code: string;
+  confidence: string;
+  details:
+    | {
+        category: string | undefined;
+        ip: string | undefined;
+        matches: string | undefined;
+        source: string | undefined;
+      }
+    | undefined;
 }
