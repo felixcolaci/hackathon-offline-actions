c5a4a1fb5147d376bdac64f0695788e6e34c081a
Felix Colaci <felix.colaci@okta.com>
2023-10-18 10:39:36 +0200

    patch request with primitive type

--- types/Event/types/Request.ts
+++ types/Event/types/Request.ts
@@ -2,46 +2,46 @@
  * Details about the request that initiated the transaction.
  */
 export interface EventRequest {
-    /**
-     * The body of the POST request. This data will only be available during refresh token and Client Credential Exchange flows.
-     */
-    body: { [key: string]: any },
-    geoip: EventRequestGeoip,
-    /**
-     * The hostname that is being used for the authentication flow.
-     */
-    hostname: String | undefined,
-    /**
-     * The originating IP address of the request.
-     */
-    ip: String,
-    /**
-     * The language requested by the browser.
-     */
-    language: String | undefined,
-    /**
-     * The HTTP method used for the request.
-     */
-    method: String,
-    /**
-     * The query string parameters sent to the authorization request.
-     */
-    query: { [key: string]: any },
-    /**
-     * The value of the User-Agent header received when initiating the transaction.
-     */
-    user_agent: String | undefined
+  /**
+   * The body of the POST request. This data will only be available during refresh token and Client Credential Exchange flows.
+   */
+  body: { [key: string]: any };
+  geoip: EventRequestGeoip;
+  /**
+   * The hostname that is being used for the authentication flow.
+   */
+  hostname: string | undefined;
+  /**
+   * The originating IP address of the request.
+   */
+  ip: string;
+  /**
+   * The language requested by the browser.
+   */
+  language: string | undefined;
+  /**
+   * The HTTP method used for the request.
+   */
+  method: string;
+  /**
+   * The query string parameters sent to the authorization request.
+   */
+  query: { [key: string]: any };
+  /**
+   * The value of the User-Agent header received when initiating the transaction.
+   */
+  user_agent: string | undefined;
 }
 
 interface EventRequestGeoip {
-    cityName: String | undefined,
-    continentCode: String | undefined,
-    countryCode: String | undefined,
-    countryCode3: String | undefined,
-    countryName: String | undefined,
-    latitude: String | undefined,
-    longitude: String | undefined,
-    subdivisionCode: String | undefined,
-    subdivisionName: String | undefined,
-    timeZone: String | undefined
+  cityName: string | undefined;
+  continentCode: string | undefined;
+  countryCode: string | undefined;
+  countryCode3: string | undefined;
+  countryName: string | undefined;
+  latitude: string | undefined;
+  longitude: string | undefined;
+  subdivisionCode: string | undefined;
+  subdivisionName: string | undefined;
+  timeZone: string | undefined;
 }
