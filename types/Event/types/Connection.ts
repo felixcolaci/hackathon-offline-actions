c0a2950a60024e4f368219b2153fa3e693d82bd5
Felix Colaci <felix.colaci@okta.com>
2023-10-18 10:40:23 +0200

    patch connection with primitive type

--- types/Event/types/Connection.ts
+++ types/Event/types/Connection.ts
@@ -1,18 +1,18 @@
 export interface EventConnection {
-    /**
-     * The connection's unique identifier.
-     */
-    id: String,
-    /**
-     * Metadata associated with the connection.
-     */
-    metadata: { [key: string]: String } | undefined,
-    /**
-     * The name of the connection used to authenticate the user (such as twitter or some-g-suite-domain).
-     */
-    name: String,
-    /**
-     * The type of connection. For social connections, event.connection.strategy === event.connection.name. For enterprise connections, the strategy is waad (Windows Azure AD), ad (Active Directory/LDAP), auth0 (database connections), and so on.
-     */
-    strategy: String
+  /**
+   * The connection's unique identifier.
+   */
+  id: string;
+  /**
+   * Metadata associated with the connection.
+   */
+  metadata: { [key: string]: string } | undefined;
+  /**
+   * The name of the connection used to authenticate the user (such as twitter or some-g-suite-domain).
+   */
+  name: string;
+  /**
+   * The type of connection. For social connections, event.connection.strategy === event.connection.name. For enterprise connections, the strategy is waad (Windows Azure AD), ad (Active Directory/LDAP), auth0 (database connections), and so on.
+   */
+  strategy: string;
 }
